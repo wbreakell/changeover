6af2a1d68d090526589b93b69ea95ed602a1e8c4
Walter Breakell <walterjbreakell@gmail.com>
2017-04-01 16:34:06 -0600

    Create Logo component

---
+++ src/components/Logo.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Logo = styled.a`
+  color: #FFF;
+  font-size: 1.5rem;
+  font-weight: 700;
+  text-decoration: none;
+
+  @media (min-width: 768px) {
+    font-size: 2.25rem;
+  }
+`;
+
+export default Logo;
