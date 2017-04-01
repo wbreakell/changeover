5050d22fbbb27be859c71273e42b0ac9d60f1809
Walter Breakell <walterjbreakell@gmail.com>
2017-03-31 23:35:16 -0600

    Create Container component

---
+++ src/components/Container.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 100%;
+  padding-left: 25px;
+  padding-right: 25px;
+
+  @media (min-width: 576px) {
+    width: 540px;
+  }
+
+  @media (min-width: 768px) {
+    width: 720px;
+  }
+
+  @media (min-width: 992px) {
+    width: 960px;
+  }
+
+  @media (min-width: 1200px) {
+    width: 1140px;
+  }
+`;
+
+export default Container;
