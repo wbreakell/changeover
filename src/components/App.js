24891663c77223d0a33edb942f78d3aa764db60f
Walter Breakell <walterjbreakell@gmail.com>
2017-04-05 16:36:58 -0600

    Create Headline component

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Container from './Container';
 import Header from './Header';
+import Headline from './Headline';
 import Logo from './Logo';
 
 const App = () => (
   <Header>
     <Container>
       <Logo href="#">C</Logo>
+      <Headline>Simplified Foreign Currency</Headline>
     </Container>
   </Header>
 );
---
+++ src/components/Headline.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Headline = styled.h1`
+  color: #FFF;
+  font-size: 1.5rem;
+  font-weight: 500;
+
+  @media (min-width: 768px) {
+    font-size: 2.25rem;
+  }
+`;
+
+export default Headline;
