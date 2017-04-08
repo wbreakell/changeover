32890b254b3b6a8496ab484b6c32a9e8f4dad471
Walter Breakell <walterjbreakell@gmail.com>
2017-04-08 17:35:35 -0600

    Create FooterLink component

---
+++ src/components/FooterLink.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterLink = styled.a`
+  color: #D8D8D8;
+  font-size: 0.875rem;
+  margin-left: 1.5rem;
+  text-decoration: none;
+`;
+
+export default FooterLink;
