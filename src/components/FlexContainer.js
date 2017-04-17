54a53f3e7e7310b2813f8e7e42fbbbdf9328981e
Walter Breakell <walterjbreakell@gmail.com>
2017-04-16 21:40:08 -0600

    Create FlexContainer component

---
+++ src/components/FlexContainer.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FlexContainer = styled.div`
+  display: flex;
+  ${props => props.alignContentXS && `align-content: ${props.alignContentXS};`}
+  ${props => props.alignItemsXS && `align-items: ${props.alignItemsXS};`}
+  ${props => props.flexDirectionXS && `flex-direction: ${props.flexDirectionXS};`}
+  ${props => props.flexWrapXS && `flex-wrap: ${props.flexWrapXS};`}
+  ${props => props.justifyContentXS && `justify-content: ${props.justifyContentXS};`}
+
+  @media (min-width: 576px) {
+    ${props => props.alignContentSM && `align-content: ${props.alignContentSM};`}
+    ${props => props.alignItemsSM && `align-items: ${props.alignItemsSM};`}
+    ${props => props.flexDirectionSM && `flex-direction: ${props.flexDirectionSM};`}
+    ${props => props.flexWrapSM && `flex-wrap: ${props.flexWrapSM};`}
+    ${props => props.justifyContentSM && `justify-content: ${props.justifyContentSM};`}
+  }
+
+  @media (min-width: 768px) {
+    ${props => props.alignContentMD && `align-content: ${props.alignContentMD};`}
+    ${props => props.alignItemsMD && `align-items: ${props.alignItemsMD};`}
+    ${props => props.flexDirectionMD && `flex-direction: ${props.flexDirectionMD};`}
+    ${props => props.flexWrapMD && `flex-wrap: ${props.flexWrapMD};`}
+    ${props => props.justifyContentMD && `justify-content: ${props.justifyContentMD};`}
+  }
+
+  @media (min-width: 992px) {
+    ${props => props.alignContentLG && `align-content: ${props.alignContentLG};`}
+    ${props => props.alignItemsLG && `align-items: ${props.alignItemsLG};`}
+    ${props => props.flexDirectionLG && `flex-direction: ${props.flexDirectionLG};`}
+    ${props => props.flexWrapLG && `flex-wrap: ${props.flexWrapLG};`}
+    ${props => props.justifyContentLG && `justify-content: ${props.justifyContentLG};`}
+  }
+
+  @media (min-width: 1200px) {
+    ${props => props.alignContentXL && `align-content: ${props.alignContentXL};`}
+    ${props => props.alignItemsXL && `align-items: ${props.alignItemsXL};`}
+    ${props => props.flexDirectionXL && `flex-direction: ${props.flexDirectionXL};`}
+    ${props => props.flexWrapXL && `flex-wrap: ${props.flexWrapXL};`}
+    ${props => props.justifyContentXL && `justify-content: ${props.justifyContentXL};`}
+  }
+`;
+
+export default FlexContainer;
