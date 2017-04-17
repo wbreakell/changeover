29d07b24a863621b318a1c8b91efb5010844bc18
Walter Breakell <walterjbreakell@gmail.com>
2017-04-16 21:40:57 -0600

    Create FlexItem component

---
+++ src/components/FlexItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FlexItem = styled.div`
+  ${props => props.alignSelfXS && `align-self: ${props.alignSelfXS};`}
+  ${props => props.flexBasisXS && `flex-basis: ${props.flexBasisXS};`}
+  ${props => props.flexGrowXS && `flex-grow: ${props.flexGrowXS};`}
+  ${props => props.flexShrinkXS && `flex-shrink: ${props.flexShrinkXS};`}
+  ${props => props.orderXS && `order: ${props.orderXS};`}
+
+  @media (min-width: 576px) {
+    ${props => props.alignSelfSM && `align-self: ${props.alignSelfSM};`}
+    ${props => props.flexBasisSM && `flex-basis: ${props.flexBasisSM};`}
+    ${props => props.flexGrowSM && `flex-grow: ${props.flexGrowSM};`}
+    ${props => props.flexShrinkSM && `flex-shrink: ${props.flexShrinkSM};`}
+    ${props => props.orderSM && `order: ${props.orderSM};`}
+  }
+
+  @media (min-width: 768px) {
+    ${props => props.alignSelfMD && `align-self: ${props.alignSelfMD};`}
+    ${props => props.flexBasisMD && `flex-basis: ${props.flexBasisMD};`}
+    ${props => props.flexGrowMD && `flex-grow: ${props.flexGrowMD};`}
+    ${props => props.flexShrinkMD && `flex-shrink: ${props.flexShrinkMD};`}
+    ${props => props.orderMD && `order: ${props.orderMD};`}
+  }
+
+  @media (min-width: 992px) {
+    ${props => props.alignSelfLG && `align-self: ${props.alignSelfLG};`}
+    ${props => props.flexBasisLG && `flex-basis: ${props.flexBasisLG};`}
+    ${props => props.flexGrowLG && `flex-grow: ${props.flexGrowLG};`}
+    ${props => props.flexShrinkLG && `flex-shrink: ${props.flexShrinkLG};`}
+    ${props => props.orderLG && `order: ${props.orderLG};`}
+  }
+
+  @media (min-width: 1200px) {
+    ${props => props.alignSelfXL && `align-self: ${props.alignSelfXL};`}
+    ${props => props.flexBasisXL && `flex-basis: ${props.flexBasisXL};`}
+    ${props => props.flexGrowXL && `flex-grow: ${props.flexGrowXL};`}
+    ${props => props.flexShrinkXL && `flex-shrink: ${props.flexShrinkXL};`}
+    ${props => props.orderXL && `order: ${props.orderXL};`}
+  }
+`;
+
+export default FlexItem;
