8c66c729d2ae336af0faf00bc8418b8a60cea0fd
Walter Breakell <walterjbreakell@gmail.com>
2017-04-16 21:55:42 -0600

    Adjust the left and right margin on the FooterLink component

--- src/components/FooterLink.js
+++ src/components/FooterLink.js
@@ -4,8 +4,14 @@ import styled from 'styled-components';
 const FooterLink = styled.a`
   color: #D8D8D8;
   font-size: 0.875rem;
-  margin-left: 1.5rem;
+  margin-right: 1.5rem;
   text-decoration: none;
+
+  @media (min-width: 576px) {
+    margin-left: 1.5rem;
+    margin-right: 0;
+  }
+
 `;
 
 export default FooterLink;
