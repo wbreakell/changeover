9baac35202bf0b2dba39f9f7a08bbc8888826d96
Walter Breakell <walterjbreakell@gmail.com>
2017-04-23 20:35:08 -0600

    Adjust the bottom and top margin on the Headline component

--- src/components/Headline.js
+++ src/components/Headline.js
@@ -5,9 +5,13 @@ const Headline = styled.h1`
   color: #FFF;
   font-size: 1.5rem;
   font-weight: 500;
+  margin-bottom: 6rem;
+  margin-top: 2rem;
 
   @media (min-width: 768px) {
     font-size: 2.25rem;
+    margin-bottom: 8rem;
+    margin-top: 4rem;
   }
 `;
 
