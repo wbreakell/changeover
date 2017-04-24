50f992f196da8344cf3064cb1ffa15c543bc759f
Walter Breakell <walterjbreakell@gmail.com>
2017-04-23 20:29:20 -0600

    Create Label component

---
+++ src/components/Label.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+  color: #40434B;
+  display: block;
+  font-size: 0.875rem;
+  font-weight: 700;
+  margin-bottom: 0.5rem;
+  text-transform: uppercase;
+`;
+
+export default Label;
