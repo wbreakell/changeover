cf0592f262eea8005703b4fba839e2dee7793c1d
Walter Breakell <walterjbreakell@gmail.com>
2017-04-29 20:28:39 -0600

    Create setBaseAmount action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,11 @@
-import { SET_BASE_CURRENCY } from '../constants/actionTypes';
+import * as types from '../constants/actionTypes';
+
+export const setBaseAmount = amount => ({
+  type: types.SET_BASE_AMOUNT,
+  amount
+});
 
 export const setBaseCurrency = currency => ({
-  type: SET_BASE_CURRENCY,
+  type: types.SET_BASE_CURRENCY,
   currency
 });
--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -2,6 +2,13 @@ import * as types from '../constants/actionTypes';
 import * as actions from './index';
 
 describe('Action creators', () => {
+  test('create SET_BASE_AMOUNT action', () => {
+    expect(actions.setBaseAmount(15)).toEqual({
+      type: types.SET_BASE_AMOUNT,
+      amount: 15
+    });
+  });
+
   test('create SET_BASE_CURRENCY action', () => {
     expect(actions.setBaseCurrency('GBP')).toEqual({
       type: types.SET_BASE_CURRENCY,
