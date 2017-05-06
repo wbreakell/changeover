fdaebef423db519be489647938d3971fcc32c943
Walter Breakell <walterjbreakell@gmail.com>
2017-05-05 21:48:55 -0600

    Make the redux store available while testing the App component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import reducer from '../reducers';
 
 describe('App', () => {
   test('renders without crashing', () => {
+    const store = createStore(reducer);
     const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div
+    );
   });
 });
