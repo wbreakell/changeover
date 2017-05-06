7d604a8b1c52f84f6b5a84fac854dacf2eb0d9f3
Walter Breakell <walterjbreakell@gmail.com>
2017-05-05 21:37:16 -0600

    Create Input component

---
+++ src/components/Input.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import { media } from '../styles/utilities/media';
+
+const Input = styled.input`
+  background-color: #F9F9F9;
+  border: 0;
+  color: #40434B;
+  font-size: 2.25rem;
+  font-weight: 400;
+  margin-bottom: 1.5rem;
+  padding: 0.5rem 1rem;
+  width: 100%;
+
+  ${media.md`
+    font-size: 3rem;
+    font-weight: 300;
+    margin-bottom: 0;
+  `}
+`;
+
+export default Input;
