62e48501fb231ec2886662f6bd4ba8018a0f63d6
Walter Breakell <walterjbreakell@gmail.com>
2017-06-07 23:40:28 -0600

    Flag constants for flow type checking

--- src/constants/actionTypes.js
+++ src/constants/actionTypes.js
@@ -1,2 +1,4 @@
+// @flow
+
 export const SET_BASE_AMOUNT = 'SET_BASE_AMOUNT';
 export const SET_BASE_CURRENCY = 'SET_BASE_CURRENCY';
--- src/constants/currencies.js
+++ src/constants/currencies.js
@@ -1,3 +1,5 @@
+// @flow
+
 const currencies = {
   AUD: 'AUD',
   BGN: 'BGN',
